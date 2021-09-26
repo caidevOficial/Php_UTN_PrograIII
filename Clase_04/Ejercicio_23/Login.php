494e0da607a241e5ee70e7549a34b8e2690c2be8
CaidevOficial <falcone.facu@gmail.com>
2021-09-25 22:41:05 -0300

    Ejercicio_23

--- Clase_04/Ejercicio_23/Login.php
+++ Clase_04/Ejercicio_23/Login.php
@@ -1,21 +1,48 @@
 <?php
 
-require_once 'usuario.php';
+require_once 'Users.php';
+require_once 'UploadManager.php';
 
 $option = $_GET['task'];
 $name = $_POST['name'];
 $password = $_POST['password'];
 $email = $_POST['email'];
 
+//--- Sets the timezone to use. ---//
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+$newID = 0;
+$firstRegister = false;
+
+//--- Instance of the class UploadManager. ---//
+$UpManager = new UploadManager($_FILES);
 var_dump($option);
 
 switch ($option) {
     case 'register':
-        $user = new Usuario($name, $password, $email);
-        if($user->SaveToJSON()){
-            echo "Usuario guardado correctamente";
-        }else{
+        if (!$firstRegister) {
+            $firstRegister = true;
+            $newID = rand(1, 10001);
+        } else {
+            $newID +=1;
+        }
+        $registerDate = new DateTime("now");
+        $user = new Usuario($newID, $name, $password, $email, $registerDate->format('d-m-Y H:m:s'));
+        //--- Gets the old array of users from the file. ---//
+        $myArray = Usuario::ReadJSON();
+
+        //--- Adds the new user to the array. ---//
+        array_push($myArray, $user);
+        
+        //--- Saves the New Array of Users in a JSON file. ---//
+        if ($user->SaveToJSON($myArray)) {
+            echo $user->errorMessageOfJSON().'<br>';
+            echo "Usuario guardado correctamente<br>";
+        } else {
             echo "Error al guardar el usuario";
+        }
+
+        if ($UpManager->saveFileIntoDir($_FILES)) {
+            echo "Archivo guardado correctamente<br>";
         }
         break;
 }
