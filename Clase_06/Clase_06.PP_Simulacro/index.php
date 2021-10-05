78ab078e3fd3b38640c21e2ae7fd5d1da108e307
CaidevOficial <falcone.facu@gmail.com>
2021-10-05 01:39:00 -0300

    PP_Parte_01

---
+++ Clase_06/Clase_06.PP_Simulacro/index.php
@@ -0,0 +1,49 @@
+<?php
+/* 
+ * MIT License
+ *
+ * Copyright (C) 2021 <FacuFalcone - CaidevOficial>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Facundo Falcone <CaidevOficial> 
+*/
+
+$_DELETE = array();
+$method = $_SERVER['REQUEST_METHOD'];
+
+//--- Sets the timezone to use. ---//
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+switch ($method) {
+    case 'GET':
+        switch (key($_GET)) {
+            case 'Cargar':
+                include 'PizzaCarga.php';
+                break;
+            case 'Consultas':
+                include 'PizzaConsultar.php';
+                break;
+            }
+        break;
+    case 'POST':
+        switch (key($_GET)) {
+            case 'Consultas':
+                include 'PizzaConsultar.php';
+                break;
+            }
+        break;
+}
+    
+?>
