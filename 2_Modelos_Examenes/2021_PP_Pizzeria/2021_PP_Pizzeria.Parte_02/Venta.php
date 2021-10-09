91ea1a89d5af846bc02997fbea574da8bb60654e
CaidevOficial <falcone.facu@gmail.com>
2021-10-08 23:32:13 -0300

    PP_Pizzeria - Parte_02 [Terminado]

--- 2_Modelos_Examenes/2021_PP_Pizzeria/2021_PP_Pizzeria.Parte_02/Venta.php
+++ 2_Modelos_Examenes/2021_PP_Pizzeria/2021_PP_Pizzeria.Parte_02/Venta.php
@@ -34,58 +34,104 @@ class Venta{
 
     //--- Getters ---//
     
+    /**
+     * Get the value of _date.
+     * @return date _date
+     */
     public function getDate(){
         return $this->_date;
     }
 
+    /**
+     * Get the value of _userEmail.
+     * @return string _userEmail
+     */
     public function getUserEmail(){
         return $this->_userEmail;
     }
 
+    /**
+     * Get the value of _pizzaFlavor.
+     * @return string _pizzaFlavor
+     */
     public function getPizzaFlavor(){
         return $this->_pizzaFlavor;
     }
 
+    /**
+     * Get the value of _pizzaType.
+     * @return string _pizzaType
+     */
     public function getPizzaType(){
         return $this->_pizzaType;
     }
 
+    /**
+     * Get the value of _pizzaAmount.
+     * @return int The amount of pizzas ordered.
+     */
     public function getPizzaAmount(){
         return $this->_pizzaAmount;
     }
 
     //--- Setters ---//
 
+    /**
+     * Set the value of _date.
+     * @param date $date The date of the order.
+     */
     public function setDate($date){
         if (!empty($date)) {
             $this->_date = $date;
         }
     }
 
+    /**
+     * Set the value of _userEmail.
+     * @param string $userEmail The email of the user who made the order.
+     */
     public function setUserEmail($userEmail){
         if (!empty($userEmail)) {
             $this->_userEmail = $userEmail;
         }
     }
 
+    /**
+     * Set the value of _pizzaFlavor.
+     * @param string $pizzaFlavor The flavor of the pizza ordered.
+     */
     public function setPizzaFlavor($pizzaFlavor){
         if (!empty($pizzaFlavor)) {
             $this->_pizzaFlavor = $pizzaFlavor;
         }
     }
 
+    /**
+     * Set the value of _pizzaType.
+     * @param string $pizzaType The type of the pizza ordered.
+     */
     public function setPizzaType($pizzaType){
         if (!empty($pizzaType)) {
             $this->_pizzaType = $pizzaType;
         }
     }
 
+    /**
+     * Sets the amount of pizzas ordered.
+     * @param int $pizzaAmount The amount of pizzas ordered.
+     */
     public function setPizzaAmount($pizzaAmount){
         if (!empty($pizzaAmount) && is_numeric($pizzaAmount)) {
             $this->_pizzaAmount = $pizzaAmount;
         }
     }
 
+    /**
+     * Sets the values of the object Venta.
+     * @param string $vEmail User email.
+     * @param Pizza $pizza Pizza object.
+     * @return Venta object with the values setted.
+     */
     public static function CreateVenta($vEmail, $pizza){
         $venta = new Venta();
         $venta->setDate(date("Y-m-d"));
@@ -99,6 +145,11 @@ class Venta{
     //--- PDO Methods ---//
 
 
+    /**
+     * Inserts a new Venta into the database.
+     * @param DataAccess $DAO  The Data Access Object.
+     * @return bool  True if the insert was successful, false otherwise.
+     */
     public function insertIntoDB($DAO){
         $sql = "INSERT INTO venta (fecha, correo_usuario, sabor_pizza, tipo_pizza, cantidad_pizza) VALUES (:fecha, :email, :sabor, :tipo, :cantidad);";
         $query = $DAO->getQuery($sql);
@@ -111,7 +162,6 @@ class Venta{
 
         return $DAO->ReturnLastIDInserted();
     }
-
 
 }
 
